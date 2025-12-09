f2a34e1f0f46a38b5c2f1aedc5e473e0229b7a64
Administrator <maidinhtuan123456@gmail.com>
2025-12-09 17:42:15 +0700

    test2

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,5 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-with DAG('hello_world_gitlab22', start_date=datetime(2023, 1, 1), schedule_interval=None) as dag:
+with DAG('hello_world_gitlab2', start_date=datetime(2023, 1, 1), schedule_interval=None) as dag:
     t1 = BashOperator(task_id='say_hello', bash_command='echo "Hello from GitLab CI/CD"')
