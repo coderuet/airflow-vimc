26eafabd40430ada62f53048c48773af9044a955
coderuet.2018 <coderuet.2018@gmail.com>
2026-01-08 03:22:15 +0700

    [fix]: change process to backfill

--- dags/ingest.py
+++ dags/ingest.py
@@ -55,7 +55,7 @@ def build_spark_config(**context):
         arguments = [
             context["dag"].dag_id,
             context["run_id"],
-            "REPROCESS",
+            "BACKFILL",
             start_date,
             end_date,
         ]
