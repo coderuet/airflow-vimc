54315f51883d108ad2e4aa42c61bb87a38ab4bb6
Administrator <admin@example.com>
2026-01-12 11:12:55 +0700

    Update 12 files
    - /dags/spark-apps/spark-pi.yaml
    - /check_system_info.py
    - /hello_world.py
    - /hello_world2.py
    - /spark_k8s_dag.py
    - /dags/spark_base_demo.py
    - /dags/spark-base-demo.py
    - /dags/test_spark_job.py
    - /dags/raw_to_bronze_job.py
    - /dags/ingest_to_bronze_job.py
    - /dags/ingest_to_bronze.py
    - /dags/ingest.py

--- dags/ingest.py
+++ dags/ingest.py
@@ -42,7 +42,7 @@ def done():
 
 
 with DAG(
-    dag_id="spark_ingest_demo",
+    dag_id="cfs_ingest_demo",
     default_args=default_args,
     # schedule="0 2 * * *",
     start_date=pendulum.datetime(2025, 11, 24, tz="Asia/Ho_Chi_Minh"),
